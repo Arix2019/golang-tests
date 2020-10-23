4006caa54f8efc3264ce5edd23fb0ded5f19670a
Arix2019 <arix0001@gmail.com>
2020-10-23 11:15:46 -0300

    adicionado o teste para ano bissexto

--- switch-calendar.go
+++ switch-calendar.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"time"
 )
 
+// global variavel
+var currentTime = time.Now() //obtem a data corrente
+
 func main() {
+	year := currentTime.Format("2006") //ano corrente
+	conv, err := strconv.Atoi(year)    //converte a data str p/ int
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for mes := 1; mes <= 12; mes++ {
 		fmt.Printf("Mês:%v\n", mes)
@@ -28,6 +39,19 @@ func main() {
 		case mes == 8:
 			for dia := 1; dia <= 31; dia++ {
 				fmt.Print(" ", dia)
+			}
+		case mes == 2:
+			// testa se o ano é bissexto ou não
+			if conv%4 == 0 {
+				for dia := 1; dia <= 29; dia++ {
+					fmt.Print(" ", dia)
+				}
+
+			} else {
+				for dia := 1; dia <= 28; dia++ {
+					fmt.Print(" ", dia)
+				}
+
 			}
 		case mes%2 == 0:
 			for dia := 1; dia <= 30; dia++ {
