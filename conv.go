bf7232c544e1ca038587dad75aa73d6abbddd342
Arix2019 <arix0001@gmail.com>
2020-10-22 22:50:42 -0300

    Arquivo criado.

---
+++ conv.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func main() {
+	//int para string
+	num := 12
+	fmt.Printf("O valor (%v) é do tipo: (%T).\n", num, num)
+	x := strconv.Itoa(num)
+	fmt.Printf("Agora (%v) passou a ser do tipo: (%T).\n", x, x)
+	//string para int
+	y, err := strconv.Atoi(x)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("E agora (%v) voltou a ser do tipo: (%T).\n", y, y)
+}
